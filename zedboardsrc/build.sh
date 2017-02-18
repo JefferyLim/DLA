972e51f12900743a33f57c536c3498f1096ac044
JefferyLim <Jeffery.Lim@colorado.edu>
2017-02-17 20:12:26 -0700

    Reorganized directories

--- zedboardsrc/build.sh
+++ zedboardsrc/build.sh
@@ -1,3 +1,3 @@
-./dtc -I dts -O dtb -o devicetree.dtb ../source/fw/zedboard/zedboard.sdk/devicetree/system.dts
+./dtc -I dts -O dtb -o devicetree.dtb ../fw/Vivado/zedboard/zedboard.sdk/devicetree/system.dts
 
 /home/Xilinx/SDK/2016.2/bin/bootgen -image output.bif -arch zynq -o BOOT.bin -w on 
